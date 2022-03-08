039f36cab2821745db1dbf5b0a4c2beab4135f79
mguadagni <guadagnimichael@gmail.com>
2022-03-07 19:16:21 -0500

    Started Stocks API MVP

--- src/main/java/com/careerdevs/StockInfoAPI/controllers/StockController.java
+++ src/main/java/com/careerdevs/StockInfoAPI/controllers/StockController.java
@@ -1,4 +1,10 @@
 package com.careerdevs.StockInfoAPI.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class StockController {
+
 }
